5a176970d6981e07a0e8e23152ce0f02edf06921
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-18 08:37:56 +0000

    User model&controller create

---
+++ db/migrate/20210418083028_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :user_name
+      t.string :image
+      t.text :profile
+      t.string :email
+      t.string :password_digest
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_080754) do
+ActiveRecord::Schema.define(version: 2021_04_18_083028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2021_04_18_080754) do
   create_table "posts", force: :cascade do |t|
     t.string "image"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "user_name"
+    t.string "image"
+    t.text "profile"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
