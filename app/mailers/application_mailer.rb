328a3f31dc18c569210679f4a369a3eb363988aa
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-18 19:03:43 +0000

    init

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'kaori.toyonaga.1984@gmail.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :posts
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
