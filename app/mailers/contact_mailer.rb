f076f09dcb2138a0a4d77abee511bdf8fea8c47b
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-18 19:49:11 +0000

    問合せ機能追加

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
-  @contact = contact
-  mail to: "kaori.toyonaga.1984@gmail.com", subject: "問合わせ"
-end
+    @contact = contact
+    mail to: "kaori.toyonaga.1984@gmail.com", subject: "問合わせ"
+  end
 end
