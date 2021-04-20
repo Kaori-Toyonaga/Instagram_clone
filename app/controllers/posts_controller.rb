8ee9744450acfdc6ee4f772b4f35be39a40ae7d0
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-20 17:07:46 +0000

    CRUD機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,67 @@
 class PostsController < ApplicationController
-  def index
-  end
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @post = Post.new
-  end
+ def top
+ end
 
-  def create
-    Post.create(post_params)
-    redirect_to new_post_path
-  end
+ def index
+   @posts = Post.all
+ end
 
+ def new
+   @post = Post.new
+ end
 
-  private
-  def post_params
-    params.require(:post).permit(:content)
-  end
+ def create
+   @post = Post.new(post_params)
+   # @post = current_user.posts.build(post_params)
+   # if params[:back]
+   #   render :new
+   # else
+     if @post.save
+       redirect_to posts_path
+     else
+       render :new
+     end
+   # end
+ end
+
+ def show
+   # @favorite = current_user.favorites.find_by(post_id: @post.id)
+ end
+
+ def edit
+   # if @post.user == current_user
+   #   render :edit
+   # else
+   #   redirect_to posts_path
+   # end
+ end
+
+ def update
+   if @post.update(post_params)
+     redirect_to posts_path
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @post.destroy
+   redirect_to posts_path
+ end
+
+ def confirm
+   # @post = current_user.posts.build(post_params)
+   render :new if @post.invalid?
+ end
+
+ private
+ def post_params
+   params.require(:post).permit(:content, :image, :image_cache)
+ end
+
+ def set_post
+   @post = Post.find(params[:id])
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
-  resources :posts
+  resources :posts do
+    collection do
+      post :confirm
+    end
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
