64a89e8c7cbcf0fba46c2c45e3d07df2ef251f63
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-21 18:57:19 +0000

    プロフィール更新時の必須項目変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, on: :create
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, on: :create
   validates :email, uniqueness: true, on: :create
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
   has_many :posts
   has_many :favorites, dependent: :destroy
   mount_uploader :image, ImageUploader
